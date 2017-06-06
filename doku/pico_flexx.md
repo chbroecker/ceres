d0a9e752fbcda6a580ece0edb88c0416b72d0270
Christopher Bröcker <broecker.christopher@gmail.com>
2017-06-06 19:53:23 +0200

    initial commit

---
+++ doku/pico_flexx.md
@@ -0,0 +1,35 @@
+# Pico Flexx by PMDTechnologies
+
+## Installation
+*  Go to http://pmdtec.com/picofamily/software/
+* this zip contains several packages for the different OSes
+
+## Royalviewer
+* Starting RoyalViewer on MacOSX
+  * Click the RoyalViewer app in /bin 
+* Starting RoyalViewer on Linux
+  * Execute the royalviewer.sh in /bin
+* Start the Camera -> TOOLS -> Use case -> there you can change your FPS
+  * Note higher FPS severly impacts your resolution
+
+### Shortcuts
+* S
+  * Start or Stop the Camera
+* L
+  * Display log (if buttons are visible)
+* R
+  * Start or Stop Recording
+* H
+  * Hide or Show the buttons
+* ESC
+  * Quit RoyalViewer
+* 2
+  * 2D Mode
+* 3
+  * 3D Mode
+* F1/F2/F3/F4
+  * Switch Data to Gray/Distance/Uniform/Overlay
+
+## API Documentation
+* can be found in /doc/html
+* can be started with ./index.html in /doc/html
---
+++ scripts/ceres.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+
+echo ""
+echo "This is a Shell script to set up the ceres robot"
+echo "It will need Sudo access"
+
+echo ""
+echo "Setting up ceres workspace"
+mkdir -p ~/ceres/src
+cd ~/ceres
+catkin_make
+cd ~/src
+
+echo ""
+echo "Installing uos_tools"
+git clone https://github.com/uos/uos_tools.git
+
+echo ""
+echo "Installing volksbot driver"
+git clone https://github.com/uos/volksbot_driver.git
+
+echo ""
+echo "Cloning the Pico_Flexx_Driver from Github"
+git clone https://github.com/juliangaal/pico_flexx_driver.git
+cd pico_flexx_driver/royale
+
+echo ""
+echo "Copying the libroyale SDK to the pico_flexx_driver:"
+echo "Extracting the files:"
+cp ~/Downloads/libroyale.zip .
+unzip libroyale.zip
+unzip 20170127_royale_3.1.0.122.zip
+unzip libroyale-3.1.0.122-LINUX-x86-64Bit.zip
+rm *.zip
+rm *.exe
+
+echo ""
+echo "Setting up USB details"
+mv libroyale-3.1.0.122-LINUX-x86-64Bit/driver/udev/10-royale-ubuntu.rules /etc/udev/rules.d
+
+echo ""
+echo "catkin_make to apply changes"
+cd ~/ceres/
+catkin_make
+
+echo "Completed"
